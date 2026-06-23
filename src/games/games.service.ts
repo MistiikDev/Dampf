e2c8413423c6b54132b516c073282d765d1b2f15
ianord <samy.iziki@ianord.com>
2026-06-23 14:10:40 +0200

    Fixed game service using legacy methods

--- src/games/games.service.ts
+++ src/games/games.service.ts
@@ -45,9 +45,7 @@ export class GamesService extends GenericService<GameEntity> {
   }
 
   async update(userid: string, gameid: number, updateGameDto: UpdateGameDTO) {
-    const target_game = await this.gameRepository.findOne({
-      where: { gameid: gameid },
-    });
+    const target_game = await this.findEntry({ gameid: gameid });
 
     // Only let user update if it is his OWN game
     if (userid == target_game?.publisher.userid) {
